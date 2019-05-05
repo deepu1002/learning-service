833f82f6da4aa30ebc2e8d01f8f1722f5f2f4e5c
Deepak RP <deepu1002@gmail.com>
2019-05-05 10:28:32 -0400

    First

---
+++ src/main/java/com/timepass/learningservice/TimepassServiceApplication.java
@@ -0,0 +1,13 @@
+package com.timepass.learningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimepassServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimepassServiceApplication.class, args);
+	}
+
+}
